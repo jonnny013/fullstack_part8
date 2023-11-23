5c49b3688cc37a4a31890dee17baefb6224629f6
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-11-23 11:13:30 +0800

    finish practice part d

--- apollo_client_practice/src/App.jsx
+++ apollo_client_practice/src/App.jsx
@@ -1,17 +1,26 @@
-import { useQuery } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import { ALL_PERSONS } from './queries'
 import { useState } from 'react'
 import Notify from './components/Notify'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS)
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
 
   if (result.loading) {
     return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
   }
 
    const notify = message => {
@@ -20,15 +29,23 @@ const App = () => {
        setErrorMessage(null)
      }, 10000)
    }
-
-  return (
-    <>
-      <Notify errorMessage={errorMessage} />
-      <Persons persons={result.data.allPersons} />
-      <PersonForm setError={notify} />
-      <PhoneForm setError={notify} />
-    </>
-  )
+   if (!token) {
+    return (
+      <>
+        <Notify errorMessage={errorMessage} />
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    )
+   }
+     return (
+       <>
+         <Notify errorMessage={errorMessage} />
+         <button onClick={logout}>Logout</button>
+         <Persons persons={result.data.allPersons} />
+         <PersonForm setError={notify} />
+         <PhoneForm setError={notify} />
+       </>
+     )
 }
 
 export default App
--- apollo_client_practice/src/main.jsx
+++ apollo_client_practice/src/main.jsx
@@ -1,30 +1,34 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { ApolloClient, ApolloProvider, InMemoryCache, gql } from '@apollo/client'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('phonenumbers-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  }
+})
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+})
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 })
 
-const query = gql`
-  query {
-    allPersons {
-      name
-      phone
-      address {
-        street
-        city
-      }
-      id
-    }
-  }
-`
 
-client.query({ query }).then(response => {
-  console.log(response.data)
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
