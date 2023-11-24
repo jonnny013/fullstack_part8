1dacec649d3d6a9808c718121d0858e3757b9c64
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-11-24 15:04:47 +0800

    Finish exercise 8.23

--- books_authors/index.js
+++ books_authors/index.js
@@ -8,11 +8,18 @@ const express = require('express')
 const cors = require('cors')
 const http = require('http')
 
+const { WebSocketServer } = require('ws')
+const { useServer } = require('graphql-ws/lib/use/ws')
+
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 const User = require('./models/user')
 const jwt = require('jsonwebtoken')
+
+const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
+
 
 require('dotenv').config()
 
@@ -34,9 +41,28 @@ const start = async () => {
   const app = express()
   const httpServer = http.createServer(app)
 
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/',
+  })
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
+  const serverCleanup = useServer({ schema }, wsServer)
+
   const server = new ApolloServer({
-    schema: makeExecutableSchema({ typeDefs, resolvers }),
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose()
+            },
+          }
+        },
+      },
+    ],
   })
 
   await server.start()
--- books_authors/resolvers.js
+++ books_authors/resolvers.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken')
 const Author = require('./models/author')
 const Book = require('./models/book')
 const User = require('./models/user')
+const { PubSub } = require('graphql-subscriptions')
+const pubsub = new PubSub()
 
 const resolvers = {
   Query: {
@@ -86,6 +88,7 @@ const resolvers = {
           },
         })
       }
+      pubsub.publish('BOOK_ADDED', { bookAdded: book })
       return book
     },
 
@@ -155,4 +158,11 @@ const resolvers = {
       return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
     },
   },
+  Subscription: {
+    bookAdded: {
+      subscribe: () => pubsub.asyncIterator('BOOK_ADDED')
+    }
+  }
 }
+
+module.exports = resolvers
