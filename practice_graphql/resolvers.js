3048ff62c3cbd9a5bafe3d3c1525c45b4a97dd4b
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-11-27 09:46:08 +0800

    fix n+1 issue in practice

--- practice_graphql/resolvers.js
+++ practice_graphql/resolvers.js
@@ -9,11 +9,13 @@ const resolvers = {
   Query: {
     personCount: async () => Person.collection.countDocuments(),
     allPersons: async (root, args) => {
+      console.log('Person.find')
       if (!args.phone) {
-        return Person.find({})
+        return Person.find({}).populate('friendOf')
       }
 
       return Person.find({ phone: { $exists: args.phone === 'YES' } })
+        .populate('friendOf')
     },
     findPerson: async (root, args) => Person.findOne({ name: args.name }),
     me: (root, args, context) => {
@@ -27,6 +29,15 @@ const resolvers = {
         city: root.city,
       }
     },
+    friendOf: async (root) => {
+      const friends = await User.find({
+        friends: {
+          $in: [root._id]
+        }
+      })
+      console.log("User.find")
+      return friends
+    }
   },
   Mutation: {
     addPerson: async (root, args, context) => {
